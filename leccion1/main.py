53cbc19e5d64698ab9db04c8a82a98bc36959695
augustoba <yo_augusto15@hotmail.com>
2024-04-26 21:51:24 -0300

    modificando resumen

--- leccion1/main.py
+++ leccion1/main.py
@@ -14,10 +14,14 @@ print(type(miVariable))
 int(miVariable)
 miVariable = 1 > 2
 #estructura de if
+
 if miVariable:
     print("true")
 else:
     print("false")
+    # se puede usar los operadores ternarios para el if pero solo si es algo corto
+    print("true") if miVariable else print("false")
+
 #para insertar valor por consola
 #print("ingrese un valor")
 #un input devuelve siempre un string asi que debemos parsear el retorno al tipo de dato que queremos
@@ -34,9 +38,13 @@ print(f'''
 print(f"el resultado es: {resultado}")
 
 # && es and  or es igual or y la negacion es not.
+# se puede obviar el and interponiendo la variable a comparar entre los operadores
+edad = 10
+if 5 <= edad < 10:
+    print("es mayor a 5 y menor a 10")
+else:
+    print("no esta entre 5 y 10")
+
 boleano = True
 print(not boleano)
-
-
-
-
+# None es el equivalente a null en java
